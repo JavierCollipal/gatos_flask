0300dde6a9ed92ee544336664805a0143fe6ffd1
Javier Andres Collipal Aguilar <jcollipal1212@gmail.com>
2019-09-21 02:56:21 -0300

    todos app working

--- api.py
+++ api.py
@@ -1,14 +1,24 @@
 from flask import Flask
+from flask import jsonify
+from flask import request
+from flask_cors import CORS
 from flask_restful import Resource, Api
 
 app = Flask(__name__)
 api = Api(app)
+CORS(app)
+todos = []
 
-class HelloWorld(Resource):
+
+class TodoSimple(Resource):
     def get(self):
-        return {'hello': 'world'}
+        return jsonify(todos)
 
-api.add_resource(HelloWorld, '/')
+    def post(self):
+        todos.append(request.json)
+
+
+api.add_resource(TodoSimple, '/todos')
 
 if __name__ == '__main__':
     app.run(debug=True)
